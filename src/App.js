d2549b6e3c89479a1998ec2882786dcfa506334b
Yueh Wang <yuonger741@hotmail.com>
2024-01-16 16:04:18 -0800

    Small UI touches. Adding Bluey for fun.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import FinancialInputs from './components/FinancialInputs.react.js';
 import PropertyTable from './components/PropertyTable.react.js';
 import ExportButton from './components/ExportButton.react.js';
 
+import bluey from './images/bluey.webp';
+
 function App() {
   const [data, setData] = useState([]);
   const [traditionalMortgageRate, setTraditionalMortgageRate] = useState(7);
@@ -24,9 +26,12 @@ function App() {
             <FileUploader setData={setData} />
             <hr />
             <FinancialInputs data={data} setData={setData} traditionalMortgageRate={traditionalMortgageRate} setTraditionalMortgageRate={setTraditionalMortgageRate} downpayment={downpayment} setDownpayment={setDownpayment} additionalCosts={additionalCosts} setAdditionalCosts={setAdditionalCosts} />
+            <br />
+            <img src={bluey} alt="Cute" style={{ width: "100%", height: "auto" }} />
           </Col>
           <Col md={10}>
             <PropertyTable data={data} setData={setData} traditionalMortgageRate={traditionalMortgageRate} downpayment={downpayment} additionalCosts={additionalCosts} />
+            <hr />
             <ExportButton data={data} />
           </Col>
         </Row>
